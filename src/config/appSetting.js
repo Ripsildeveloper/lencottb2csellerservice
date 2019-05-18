1d49a8bc084ab5360661b9d55d796bb732d7915a
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-05-18 12:39:27 +0530

    service

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,5 +1,5 @@
 module.exports = {
-  /* productUploadPath: './images/products',
+  productUploadPath: './images/products',
   productServerPath: 'http://localhost/Lencott/products/',
   bannerUploadPath: './images/banners',
   bannerServerPath: 'http://localhost/Lencott/banners/',
@@ -8,17 +8,17 @@ module.exports = {
   logoUploadPath: 'images/logo',
   logoServerPath: 'http://localhost/Lencott/logo/',
   headerUploadPath: 'images/header',
-  headerServerPath: 'http://localhost/Lencott/header/' */
-  imageServerPath: 'http://lencott.com/admin/images/',
-  imageUploadPath: '/home/ubuntu/app/seller-app/seller-app/images/',
+  headerServerPath: 'http://localhost/Lencott/header/'
+ /*  imageServerPath: 'http://lencott.com/admin/images/',
+  imageUploadPath: '/home/ubuntu/app/seller-ui/seller-app/images/',
   productServerPath: 'http://lencott.com/admin/images/product/',
-  productUploadPath: '/home/ubuntu/app/seller-app/seller-app/images/product',
+  productUploadPath: '/home/ubuntu/app/seller-ui/seller-app/images/product',
   bannerServerPath: 'http://lencott.com/admin/images/banner/',
-  bannerUploadPath: '/home/ubuntu/app/seller-app/seller-app/images/banner',
+  bannerUploadPath: '/home/ubuntu/app/seller-ui/seller-app/images/banner',
   adsServerPath: 'http://lencott.com/admin/images/ads/',
-  adsUploadPath: '/home/ubuntu/app/seller-app/seller-app/seller-app/images/ads',
+  adsUploadPath: '/home/ubuntu/app/seller-ui/seller-app/seller-app/images/ads',
   logoServerPath: 'http://lencott.com/admin/images/logo/',
-  logoUploadPath: '/home/ubuntu/app/seller-app/seller-app/images/logo',
+  logoUploadPath: '/home/ubuntu/app/sseller-ui/seller-app/images/logo',
   headerUploadPath: 'http://lencott.com/admin/images/header/',
-  headerServerPath: '/home/ubuntu/app/seller-app/seller-app/images/header'
+  headerServerPath: '/home/ubuntu/app/seller-ui/seller-app/images/header' */
 };
